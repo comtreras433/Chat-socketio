e4a64edf84e057a777b7922072f4995a47cb1d4c
comtreras433 <comtreras433@gmail.com>
2020-04-06 20:00:11 -0400

    Updates

---
+++ precache-manifest.905eed598dbfc41aca2833d355e6b249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d509222cb67fcf43a4e2e66be96df052",
+    "url": "/Chat-socketio/index.html"
+  },
+  {
+    "revision": "28156b57963961941076",
+    "url": "/Chat-socketio/static/css/main.95084dce.chunk.css"
+  },
+  {
+    "revision": "47374a192be89856a2d6",
+    "url": "/Chat-socketio/static/js/2.81e7aa3b.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/Chat-socketio/static/js/2.81e7aa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28156b57963961941076",
+    "url": "/Chat-socketio/static/js/main.f4beda12.chunk.js"
+  },
+  {
+    "revision": "aa69aeeeaad1cb3b9c9b",
+    "url": "/Chat-socketio/static/js/runtime-main.17590197.js"
+  }
+]);
